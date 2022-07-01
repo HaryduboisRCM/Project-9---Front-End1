7e121cb2f537590b4cb49410aad6da34dbd5042a
Tom S <harydubois101@protonmail.com>
2022-07-01 10:04:25 +0100

    Updates

---
+++ precache-manifest.a5ce5fb59b9b1f0a3561779a4b0979c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890f7e8af2bef8990a0d661cba8164d1",
+    "url": "/Project-9---Front-End/index.html"
+  },
+  {
+    "revision": "fe6186635daf1d3d8f2e",
+    "url": "/Project-9---Front-End/static/css/main.7a3a1b8a.chunk.css"
+  },
+  {
+    "revision": "eec8b20d6d199bf768c9",
+    "url": "/Project-9---Front-End/static/js/2.d3427f50.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/Project-9---Front-End/static/js/2.d3427f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6186635daf1d3d8f2e",
+    "url": "/Project-9---Front-End/static/js/main.b54da7c3.chunk.js"
+  },
+  {
+    "revision": "d8977ff2c338f7aa19ba",
+    "url": "/Project-9---Front-End/static/js/runtime-main.5391a431.js"
+  },
+  {
+    "revision": "33ef4db5db435395636402565a3018ba",
+    "url": "/Project-9---Front-End/static/media/event image 1.33ef4db5.jpg"
+  }
+]);
